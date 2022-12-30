ba669e12eb6c83e7763b68c62c239668464262f4
taylorchu <tailinchu@gmail.com>
2022-12-29 23:11:45 -0800

    gjson

--- api/loki/logql.go
+++ api/loki/logql.go
@@ -1,28 +1,161 @@
 package loki
 
 import (
+	"regexp"
+	"strconv"
+	"strings"
+
 	"github.com/commentlens/loghouse/api/loki/logql/parser/bsr"
 	"github.com/commentlens/loghouse/api/loki/logql/parser/symbols"
 	"github.com/commentlens/loghouse/storage"
-	"github.com/davecgh/go-spew/spew"
+	"github.com/tidwall/gjson"
 )
 
-func logqlRead(r storage.Reader, optsf func() *storage.ReadOptions, root bsr.BSR) ([]*storage.LogEntry, bool, error) {
+func logqlRead(r storage.Reader, f func() *storage.ReadOptions, root bsr.BSR) ([]*storage.LogEntry, bool, error) {
 	var isHistogram bool
-	filter := func(e *storage.LogEntry) bool {
-		return true
-	}
-	ropts := optsf()
+	var filters []func(e *storage.LogEntry) bool
+	ropts := f()
 	logqlWalk(root, func(node bsr.BSR) {
 		switch node.Label.Slot().NT {
 		case symbols.NT_MetricQuery:
 			isHistogram = true
 		case symbols.NT_LogSelectorMember:
-			spew.Dump(node.Label)
+			key := node.GetNTChildI(0).GetTChildI(0).LiteralString()
+			op := node.GetNTChildI(1).GetTChildI(0).LiteralString()
+			val := node.GetTChildI(2).LiteralString()
+			val, err := strconv.Unquote(val)
+			if err != nil {
+				return
+			}
+			switch op {
+			case "=":
+				if ropts.Labels == nil {
+					ropts.Labels = make(map[string]string)
+				}
+				ropts.Labels[key] = val
+			case "!=":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					v, ok := e.Labels[key]
+					if !ok {
+						return false
+					}
+					return v != val
+				})
+			case "=~":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					v, ok := e.Labels[key]
+					if !ok {
+						return false
+					}
+					ok, err := regexp.MatchString(val, v)
+					if err != nil {
+						return false
+					}
+					return ok
+				})
+			case "!~":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					v, ok := e.Labels[key]
+					if !ok {
+						return false
+					}
+					ok, err := regexp.MatchString(val, v)
+					if err != nil {
+						return false
+					}
+					return !ok
+				})
+			}
 		case symbols.NT_LineFilter:
-			spew.Dump(node.Label)
+			op := node.GetNTChildI(0).GetTChildI(0).LiteralString()
+			val := node.GetTChildI(1).LiteralString()
+			val, err := strconv.Unquote(val)
+			if err != nil {
+				return
+			}
+			switch op {
+			case "|=":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					return strings.Contains(string(e.Data), val)
+				})
+			case "!=":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					return !strings.Contains(string(e.Data), val)
+				})
+			case "|~":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					ok, err := regexp.MatchString(val, string(e.Data))
+					if err != nil {
+						return false
+					}
+					return ok
+				})
+			case "!~":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					ok, err := regexp.MatchString(val, string(e.Data))
+					if err != nil {
+						return false
+					}
+					return !ok
+				})
+			}
 		case symbols.NT_LabelFilter:
-			spew.Dump(node.Label)
+			var keyParts []string
+			for i, sym := range node.GetNTChildI(1).Label.Symbols() {
+				if sym.IsNonTerminal() {
+					continue
+				}
+				keyParts = append(keyParts, node.GetNTChildI(1).GetTChildI(i).LiteralString())
+			}
+			key := strings.Join(keyParts, "")
+			op := node.GetNTChildI(2).GetTChildI(0).LiteralString()
+			val := node.GetTChildI(3).LiteralString()
+			val, err := strconv.Unquote(val)
+			if err != nil {
+				return
+			}
+			switch op {
+			case "=":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					v := gjson.GetBytes(e.Data, key)
+					if !v.Exists() {
+						return false
+					}
+					return v.String() == val
+				})
+			case "!=":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					v := gjson.GetBytes(e.Data, key)
+					if !v.Exists() {
+						return false
+					}
+					return v.String() != val
+				})
+			case "=~":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					v := gjson.GetBytes(e.Data, key)
+					if !v.Exists() {
+						return false
+					}
+					ok, err := regexp.MatchString(val, v.String())
+					if err != nil {
+						return false
+					}
+					return ok
+				})
+			case "!~":
+				filters = append(filters, func(e *storage.LogEntry) bool {
+					v := gjson.GetBytes(e.Data, key)
+					if !v.Exists() {
+						return false
+					}
+					ok, err := regexp.MatchString(val, v.String())
+					if err != nil {
+						return false
+					}
+					return !ok
+				})
+			}
 		}
 	})
 	es, err := r.Read(ropts)
@@ -31,7 +164,14 @@ func logqlRead(r storage.Reader, optsf func() *storage.ReadOptions, root bsr.BSR
 	}
 	var out []*storage.LogEntry
 	for _, e := range es {
-		if filter(e) {
+		match := true
+		for _, filter := range filters {
+			if !filter(e) {
+				match = false
+				break
+			}
+		}
+		if match {
 			out = append(out, e)
 		}
 	}
--- go.mod
+++ go.mod
@@ -9,9 +9,12 @@ require (
 	github.com/klauspost/compress v1.15.13
 	github.com/oklog/ulid/v2 v2.1.0
 	github.com/stretchr/testify v1.8.1
+	github.com/tidwall/gjson v1.14.4
 )
 
 require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
