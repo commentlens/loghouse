af61e0c1558841e6fa559d2ef917552aae85fa71
taylorchu <tailinchu@gmail.com>
2022-12-31 00:47:34 -0800

    more logs

--- api/loki/log.go
+++ api/loki/log.go
@@ -3,13 +3,17 @@ package loki
 import (
 	"net/http"
 
+	"github.com/oklog/ulid/v2"
 	"github.com/sirupsen/logrus"
 )
 
 func httpLogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			logrus.WithField("method", r.Method).WithField("url", r.URL.String()).Info("loki http request served")
+			reqID := ulid.Make().String()
+			log := logrus.WithField("method", r.Method).WithField("url", r.URL.String()).WithField("req_id", reqID)
+			log.Info("loki http request started")
+			defer log.Info("loki http request finished")
 			next.ServeHTTP(w, r)
 		},
 	)
